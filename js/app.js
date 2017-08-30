75a488e026268716fc034c04a17a600aadbe2a76
Mariamma Antony <Mariamma.Antony@aexp.com>
2017-08-30 10:11:01 +0530

    adding changes to git

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
-var Cat = function(){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Tabby");
-	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
+var initialCats = [{
+		clickCount: 0,
+		name: "Kris",
+		imgSrc: "img/cat1.jpg",
+		nickname:[
+		{name:"Toto"},
+		{name:"Tictac"}]
+	},
+	{
+		clickCount: 0,
+		name: "Kristy",
+		imgSrc: "img/cat2.jpg",
+		nickname:[
+		{name:"Toto"},
+		{name:"Tictac"}]
+	},
+	{
+		clickCount: 0,
+		name: "Snowy",
+		imgSrc: "img/cat3.jpg",
+		nickname:[
+		{name:"Toto"},
+		{name:"Tictac"}]
+	},
+	{
+		clickCount: 0,
+		name: "Shadow",
+		imgSrc: "img/cat4.jpg",
+		nickname:[
+		{name:"Toto"},
+		{name:"Tictac"}]
+	},
+	{
+		clickCount: 0,
+		name: "Mater",
+		imgSrc: "img/cat5.jpg",
+		nickname:[
+		{name:"Toto"},
+		{name:"Tictac"}]
+	}
+	];
+
+var Cat = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nickname=ko.observableArray(data.nickname);
 
 	this.level = ko.computed(function(){
 		if(this.clickCount() > 15){
@@ -12,17 +55,36 @@ var Cat = function(){
 			return "Infant";	
 		}
 	}, this);
-
-	this.nickname=ko.observableArray([
-		{name:"Toto"},
-		{name:"Tictac"}]);
 }
 
-//for simolicity we put model inside viewmodel
+//for simplicity we put model inside viewmodel
 var ViewModel = function(){
-	this.currentCat = ko.observable(new Cat());
+	var self=this;	//self points to this ViewModel
+	this.catList = ko.observableArray([]);
+	initialCats.forEach(function(catItem){
+		self.catList.push(catItem);
+	});
+
+	// var cat1 = new Cat({
+	// 	name:"Tabby",
+	// 	imgSrc:"img/cat1.jpg",
+	// 	clickCount:0,
+	// 	nickname:[
+	// 	{name:"Toto"},
+	// 	{name:"Tictac"}]
+	// });
+	//this.currentCat = ko.observable(new Cat(this.catList()[0]));
+	this.currentCat = ko.observable(this.catList()[0]);
 	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
+		// this.clickCount(this.clickCount() + 1);
+		console.log("Current clicks = " + self.currentCat().clickCount());
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.changeCurrrentCat = function(cat){
+		console.log("Clicked " + cat.name);
+		self.currentCat(cat);// = ko.observable(new Cat(cat));
+		console.log("Click count :: " + self.currentCat().clickCount());
 	};
 }
 
